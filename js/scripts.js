2ac47a9a70bd517b538487304af39ee7e3a2ae62
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2017-10-24 21:34:05 -0700

    spec behavior 1993 is false works as expected

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business or back-end logic here
+
+var leapYear = function(year) {
+  return false;
+};
+
+// jQuery user interface logic here.
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
